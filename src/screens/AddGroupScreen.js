77d7528b2af23bc77c2af519cf6618afdb1fd5e0
ahmed khyrat <ahmedkhyrat@gmail.com>
2021-10-24 20:35:21 +0200

    Adding the Add Group Screen

--- src/screens/AddGroupScreen.js
+++ src/screens/AddGroupScreen.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import {StyleSheet, View, Text, TextInput} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View, Text, TextInput, Alert} from 'react-native';
+import Constants from '../const/Constants';
+import Colors from '../../utils/Colors';
+import Buttons from '../components/Buttons';
+import Utility from '../../utils/Utility';
+import Strings from '../const/String';
+import firestore from '@react-native-firebase/firestore';
 
-const AddGroupScreen = () => {
+const AddGroupScreen = props => {
+  const {navigation} = props;
+  const [groupName, setGroupName] = useState('');
+  const [error, setError] = useState('');
+
+  handleSubmit = () => {
+    const isValidText = Utility.isValidField(groupName);
+    if (isValidText) {
+      setError('');
+      firestore().collection('groups').add({
+        groupName: {groupName},
+      });
+      navigation.navigate('GroupsScreen');
+    } else {
+      setError(Strings.EmptyTextField);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}> Add Group Screen</Text>
+      <Text style={styles.header}> Add Group Screen</Text>
+      <Text style={styles.errorText}> {error}</Text>
+      <TextInput
+        style={styles.textInput}
+        autoCorrect={false}
+        placeholder={'Enter Group Name'}
+        value={groupName}
+        onChangeText={text => {
+          setGroupName(text);
+        }}
+        // onEndEditing={evaluateText}
+      />
+
+      <Buttons title={'Add Group'} onPress={handleSubmit} />
     </View>
   );
 };
@@ -16,10 +52,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#ebebeb',
   },
-  text: {
+  header: {
     color: '#101010',
     fontSize: 24,
     fontWeight: 'bold',
+  },
+  textInput: {
+    height: Constants.screenHeight * 0.06,
+    width: Constants.screenWidth * 0.85,
+    marginTop: 5,
+    marginBottom: 10,
+    borderColor: Colors.black,
+    borderWidth: 1,
+    justifyContent: 'center',
+    backgroundColor: Colors.smoke,
+  },
+  errorText: {
+    fontSize: 12,
+    color: Colors.red,
+    marginBottom: 5,
+    marginHorizontal: 20,
   },
 });
 
--- utils/Utility.js
+++ utils/Utility.js
@@ -2,6 +2,7 @@ export default class Utility {
   // checking the length of the input and removing the spaces (using trim)
   static isValidField = term => {
     let length = term.trim().length;
+
     return length > 0 ? true : false;
   };
 
